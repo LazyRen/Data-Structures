93b064d09328233f9e87d0d91786fd277691781f
DaeIn Lee <daein0lee@gmail.com>
2020-08-15 17:33:53 +0900

    add iterator version

--- quicksort.h
+++ quicksort.h
@@ -12,24 +12,37 @@ void swap(T& a, T& b) {
  * Sort all elements in arr[]; range of closed interval [left, right].
  */
 template <typename T>
-void quickSort(T arr[], int left, int right) {
-  if(left >= right)
-    return;
+void sort(T arr[], int left, int right) {
+  if (left >= right) return;
 
-  T pivot = arr[(left+right) / 2];
+  T pivot = arr[(left + right) / 2];
   int l = left, r = right;
 
-  while(l <= r) {
-    while(arr[l] < pivot)
-      l++;
-    while(arr[r] > pivot)
-      r--;
-    if(l <= r)
-      swap(arr[l++],arr[r--]);
+  while (l <= r) {
+    while (arr[l] < pivot) l++;
+    while (arr[r] > pivot) r--;
+    if (l <= r) swap(arr[l++], arr[r--]);
   }
 
-  quickSort(arr, left, r);
-  quickSort(arr, l, right);
+  sort(arr, left, r);
+  sort(arr, l, right);
+}
+
+/*
+ * Sort all elements in [arr, end). use it with iterator begin() & end().
+ */
+template <typename T>
+void sort(T* arr, T* end) {
+  int l = 0, r = (end - arr) - 1;
+  if (l >= r) return;
+  T pivot = arr[(l + r) / 2];
+  while (l <= r) {
+    while (arr[l] < pivot) l++;
+    while (arr[r] > pivot) r--;
+    if (l <= r) swap(arr[l++], arr[r--]);
+  }
+  sort(arr, arr + l);
+  sort(arr + l, end);
 }
 
 #endif
